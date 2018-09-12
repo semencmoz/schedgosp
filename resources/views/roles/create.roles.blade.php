94e6221502a4525c87882d769cd477a962d70504
semencmoz <semencmoz@gmail.com>
2018-09-12 12:00:05 +0300

    v0.007 added roles templates +added some controllers +added route from roles creater to roles controller

---
+++ resources/views/roles/create.roles.blade.php
@@ -0,0 +1,40 @@
+@extends('roles')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <br />
+            <h3 aling="center">Создать роль</h3>
+            <br />
+            @if(count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            @if(\Session::has('success'))
+                <div class="alert alert-success">
+                    <p>{{ \Session::get('success') }}</p>
+                </div>
+            @endif
+            <form method="post" action="{{url('student')}}">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <input type="text" name="name" class="form-control" placeholder="Введите азвание роли" />
+                </div>
+                <div class="form-group">
+                    <input type="text" name="role_type" class="form-control" placeholder="Введите тип роли" />
+                </div>
+                <div class="form-group">
+                    <input type="text" name="role_type" class="form-control" placeholder="Введите тип роли" />
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" />
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection

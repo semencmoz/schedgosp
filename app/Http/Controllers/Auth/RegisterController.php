4b21de3d9163f4d0b093834e496f19b73686a4e9
semencmoz <semencmoz@gmail.com>
2018-09-12 09:47:29 +0300

    v0.002 +конфигурация БД +миграции 4х таблиц +отключил обязательный имейл

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'string|email|max:255',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
